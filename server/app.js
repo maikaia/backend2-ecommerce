1a86068df8d1d87ad71968d81a4afb5b39632d29
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-04 10:05:55 +0200

    Move connection logic to server.js

--- server/app.js
+++ server/app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 
 const { productRoutes } = require("./controllers/products.js");
 
 const app = express();
-const PORT = 5000;
-const MONGODB_URL = "mongodb://127.0.0.1/ecommerce";
 
 app.use(cors());
 
@@ -16,8 +13,4 @@ app.get("/", (req, res) => {
 
 app.use("/products", productRoutes);
 
-mongoose.connect(MONGODB_URL);
-
-app.listen(PORT, () => {
-  console.log(`Started Express server on port ${PORT}`);
-});
+module.exports = app;
