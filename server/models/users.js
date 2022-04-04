7b630f09a26f5c5b34e34b26e483bb0bda4fd9c0
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-04 11:46:18 +0200

    Add User model

---
+++ server/models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.modifiedPaths().includes("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+const createUser = async (user) => {
+  return await User.create(user);
+};
+
+module.exports = { createUser };
