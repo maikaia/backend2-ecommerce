0e2a65b4f19cc207a349d6a1d193b66fcc4856ae
Nils-Olov Olsson <nolle_olsson_@hotmail.com>
2022-04-13 10:13:42 +0200

    Read config from process.env

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+const dotenv = require("dotenv")
+dotenv.config()
 const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const PORT = 5000;
-const MONGODB_URL = "mongodb://127.0.0.1/ecommerce";
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_RUL;
 
 mongoose.connect(MONGODB_URL);
 app.listen(PORT, () => {
